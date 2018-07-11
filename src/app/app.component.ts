6623155c0e5434a5be87e052f5fbbb34f37703ca
John Rykken <john.rykken@gmail.com>
2018-07-11 11:31:53 -0700

    Address Bulma bug. Default Bulma styles being overidden.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'My 20th Angular App';
 }
