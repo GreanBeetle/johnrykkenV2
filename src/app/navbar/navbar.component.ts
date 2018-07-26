d1af3e16e6cacdbbe0d597f76799cbc90e3d5b7d
John Rykken <john.rykken@gmail.com>
2018-07-25 17:27:22 -0700

    Toggle side menu.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,16 +13,19 @@ import { Router } from '@angular/router';
 
 export class NavbarComponent implements OnInit {
   user;
-  loggedIn: boolean;
+  loggedIn;
+  showMenu = false;
+  angularLogo = 'https://cdn-images-1.medium.com/max/1600/1*hDJlAbtIVBrvyau4d8JRLg.png';
 
   constructor(public authServ: AuthenticationService, private router: Router) {
-    this.authServ.user.subscribe(user => {
-        this.user = user;
-        console.log('ID and display name: ' + user.uid + user.displayName);
-        if (user == null) {
+    this.authServ.user.subscribe(theUser => {
+        this.user = theUser;
+        if (theUser == null) {
           this.loggedIn = false;
+          console.log ('User is null: ' + theUser + ' and here is the loggedIn var: ' + this.loggedIn);
         } else {
           this.loggedIn = true;
+          console.log ('User is true: ' + theUser.email + ' and here is the loggedIn var: ' + this.loggedIn);
         }
     });
   }
@@ -30,6 +33,15 @@ export class NavbarComponent implements OnInit {
   logout() {
     this.authServ.logout();
   }
+
+  toggleMenu() {
+    if (this.showMenu === false) {
+      this.showMenu = true;
+    } else {
+      this.showMenu = false;
+    }
+  }
+
 
   ngOnInit() {
   }
