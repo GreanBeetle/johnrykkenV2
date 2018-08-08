b1a6c4ad0c09089f14868c4228b9837b9fb02da5
John Rykken <john.rykken@gmail.com>
2018-08-07 18:23:21 -0700

    Add article model.

---
+++ src/app/models/article.model.ts
@@ -0,0 +1,10 @@
+export class Article {
+  constructor(
+    public id: string,
+    public title: string,
+    public subtitle: string,
+    public date: number,
+    public content: string,
+    public keywords: string
+    ) {}
+}
