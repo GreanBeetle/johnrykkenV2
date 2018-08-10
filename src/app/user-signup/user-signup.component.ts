a770d28ab27d72deddb38e681cdba89934834897
John Rykken <john.rykken@gmail.com>
2018-08-09 17:19:36 -0700

    Import user-auth service to user-signup component. Replace authservice createUser() with userauthservice createUser().

--- src/app/user-signup/user-signup.component.ts
+++ src/app/user-signup/user-signup.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import { MessageService } from 'primeng/api';
+import { UserAuthService } from '../user-auth.service';
 
 @Component({
   selector: 'app-user-signup',
   templateUrl: './user-signup.component.html',
   styleUrls: ['./user-signup.component.scss'],
-  providers: [ MessageService ]
+  providers: [ UserAuthService,
+               MessageService ]
 })
 export class UserSignupComponent implements OnInit {
 
   constructor(private authservice: AuthenticationService,
-              private toast: MessageService) { }
+              private toast: MessageService,
+              private userauth: UserAuthService) { }
 
   signup(displayname, email, password) {
-    this.authservice.createUser(displayname, email, password);
+    this.userauth.createUser(displayname, email, password);
     this.showToast(displayname);
   }
 
