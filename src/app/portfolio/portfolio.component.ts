8317435cabf127e2afe85f40c8f279200597c5c6
John Rykken <john.rykken@gmail.com>
2018-08-18 14:34:00 -0700

    Add scrollUp() function to portfolio page.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -9,6 +9,17 @@ export class PortfolioComponent implements OnInit {
 
   constructor() { }
 
+  scrollUp() {
+        const scrollToTop = window.setInterval(() => {
+            const pos = window.pageYOffset;
+            if (pos > 0) {
+                window.scrollTo(0, pos - 20); // how far to scroll on each step
+            } else {
+                window.clearInterval(scrollToTop);
+            }
+        }, 10);
+    }
+
   ngOnInit() {
   }
 
