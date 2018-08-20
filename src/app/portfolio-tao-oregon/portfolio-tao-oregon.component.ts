1d01f7ad502f3b7dacbc5ffd5caface9d5d17eec
John Rykken <john.rykken@gmail.com>
2018-08-20 14:16:04 -0700

    Toggle W3 modal using property 'style' binding.

--- src/app/portfolio-tao-oregon/portfolio-tao-oregon.component.ts
+++ src/app/portfolio-tao-oregon/portfolio-tao-oregon.component.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio-tao-oregon.component.scss']
 })
 export class PortfolioTaoOregonComponent {
+  display = true;
 
   constructor() { }
+
+
+  aFunction() {
+    return 'red';
+  }
+
+  displayType() {
+    if ( this.display === true ) {
+      return 'block';
+    } else if ( this.display === false ) {
+      return 'none';
+    }
+  }
+
+  toggleDisplay() {
+    alert('clicked');
+    if (this.display === false) {
+      this.display = true;
+    } else {
+      this.display = false;
+    }
+  }
 
 }
