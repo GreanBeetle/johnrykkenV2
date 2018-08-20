82724ca8c730ae33c81c7fba1d494ff1f83bf8dd
John Rykken <john.rykken@gmail.com>
2018-08-20 16:50:20 -0700

    Build west-oaks project synopsis card.

--- src/app/portfolio-west-oaks/portfolio-west-oaks.component.ts
+++ src/app/portfolio-west-oaks/portfolio-west-oaks.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-west-oaks',
   templateUrl: './portfolio-west-oaks.component.html',
   styleUrls: ['./portfolio-west-oaks.component.scss']
 })
-export class PortfolioWestOaksComponent implements OnInit {
+
+export class PortfolioWestOaksComponent {
+
+  display = false;
 
   constructor() { }
 
-  ngOnInit() {
+  displayType() {
+    if ( this.display === true ) {
+      return 'block';
+    } else if ( this.display === false ) {
+      return 'none';
+    }
+  }
+
+  toggle() {
+    if (this.display === false) {
+      this.display = true;
+    } else {
+      this.display = false;
+    }
   }
 
 }
