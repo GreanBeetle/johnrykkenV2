6558c093612bd9ef68bb10e651b36075fded44f6
John Rykken <john.rykken@gmail.com>
2018-08-20 17:11:11 -0700

    Copy HTML, SCSS, TS from tao to fomo.church.

--- src/app/portfolio-fomo-church/portfolio-fomo-church.component.ts
+++ src/app/portfolio-fomo-church/portfolio-fomo-church.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-fomo-church',
   templateUrl: './portfolio-fomo-church.component.html',
   styleUrls: ['./portfolio-fomo-church.component.scss']
 })
-export class PortfolioFomoChurchComponent implements OnInit {
+export class PortfolioFomoChurchComponent {
+
+  display = false;
 
   constructor() { }
 
-  ngOnInit() {
+  displayType() {
+    if ( this.display === true ) {
+      return 'block';
+    } else if ( this.display === false ) {
+      return 'none';
+    }
+  }
+
+  toggle() {
+    if (this.display === false) {
+      this.display = true;
+    } else {
+      this.display = false;
+    }
   }
 
 }
