d6fc8a3ff83e91bbfaf4bd265b9fb0f33df0a857
John Rykken <john.rykken@gmail.com>
2018-08-21 10:55:40 -0700

    Add portfolio section for the original Rails version of johnrykken.com

--- src/app/portfolio-john-rykken-v1/portfolio-john-rykken-v1.component.ts
+++ src/app/portfolio-john-rykken-v1/portfolio-john-rykken-v1.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-john-rykken-v1',
   templateUrl: './portfolio-john-rykken-v1.component.html',
   styleUrls: ['./portfolio-john-rykken-v1.component.scss']
 })
-export class PortfolioJohnRykkenV1Component implements OnInit {
+export class PortfolioJohnRykkenV1Component {
+
+  display = false;
 
   constructor() { }
 
-  ngOnInit() {
+  displayType() {
+      if ( this.display === true ) {
+          return 'block';
+      } else if ( this.display === false ) {
+          return 'none';
+      }
+  }
+
+  toggle() {
+    if (this.display === false) {
+      this.display = true;
+    } else {
+      this.display = false;
+    }
   }
 
 }
