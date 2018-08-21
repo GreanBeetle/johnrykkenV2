4c9a1348c71bf942eaed847df84da664b59b22ae
John Rykken <john.rykken@gmail.com>
2018-08-20 17:24:36 -0700

    Copy HTML, SCSS, TS from tao to film-fanatic.

--- src/app/portfolio-film-fanatic/portfolio-film-fanatic.component.ts
+++ src/app/portfolio-film-fanatic/portfolio-film-fanatic.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-film-fanatic',
   templateUrl: './portfolio-film-fanatic.component.html',
   styleUrls: ['./portfolio-film-fanatic.component.scss']
 })
-export class PortfolioFilmFanaticComponent implements OnInit {
+export class PortfolioFilmFanaticComponent {
+
+  display = false;
 
   constructor() { }
 
-  ngOnInit() {
+  displayType() {
+    if ( this.display === true ) {
+      return 'block';
+    } else if ( this.display === false ) {
+      return 'none';
+    }
+  }
+
+  toggle() {
+    if (this.display === false) {
+      this.display = true;
+    } else {
+      this.display = false;
+    }
   }
 
 }
