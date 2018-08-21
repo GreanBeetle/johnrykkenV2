3d4c35b078a4d10c68041d8161901a8966d0917e
John Rykken <john.rykken@gmail.com>
2018-08-21 08:07:51 -0700

    Fix github links film, bianchi, etc.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
+
+export class PortfolioComponent {
 
   constructor() { }
 
@@ -13,14 +14,11 @@ export class PortfolioComponent implements OnInit {
         const scrollToTop = window.setInterval(() => {
             const pos = window.pageYOffset;
             if (pos > 0) {
-                window.scrollTo(0, pos - 20); // how far to scroll on each step
+                window.scrollTo(0, pos - 20);
             } else {
                 window.clearInterval(scrollToTop);
             }
         }, 10);
-    }
-
-  ngOnInit() {
   }
 
 }
