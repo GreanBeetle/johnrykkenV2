8d44ea0117d409bdb41375a36fbfd5f8a9281947
John Rykken <john.rykken@gmail.com>
2018-08-20 17:29:32 -0700

    Copy HTML, SCSS, TS from tao to bianchi brew.

--- src/app/portfolio-bianchi-brew/portfolio-bianchi-brew.component.ts
+++ src/app/portfolio-bianchi-brew/portfolio-bianchi-brew.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-bianchi-brew',
   templateUrl: './portfolio-bianchi-brew.component.html',
   styleUrls: ['./portfolio-bianchi-brew.component.scss']
 })
-export class PortfolioBianchiBrewComponent implements OnInit {
+
+export class PortfolioBianchiBrewComponent {
+
+  display = false;
 
   constructor() { }
 
-  ngOnInit() {
+  displayType() {
+    if ( this.display === true ) {
+      return 'block';
+    } else if ( this.display === false ) {
+      return 'none';
+    }
+  }
+
+  toggle() {
+    if (this.display === false) {
+      this.display = true;
+    } else {
+      this.display = false;
+    }
   }
 
 }
