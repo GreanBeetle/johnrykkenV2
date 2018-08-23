152ae5435f204424a06146f566e19b1fa39348b7
John Rykken <john.rykken@gmail.com>
2018-08-23 13:59:43 -0700

    Successfully retreive and display article details on articleDetail component.

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -15,27 +15,44 @@ import { Article } from '../models/article.model';
 })
 
 export class ArticleDetailComponent implements OnInit, OnDestroy {
+  articlesCollection: AngularFirestoreCollection<Article>;
+  articles: Observable<Article[]>;
   private articleDoc: AngularFirestoreDocument<Article>;
-  article: Observable<Article>;
+  article: any;
   private sub: any;
-  ID;
+  id;
 
   constructor (private route: ActivatedRoute,
                private afs: AngularFirestore) {
-               alert(this.ID);
+      this.articlesCollection = this.afs.collection('articles');
+      this.articles = this.articlesCollection.valueChanges();
+      this.route.params.subscribe(params => {
+          this.id = params['id'];
+      });
+      this.articlesCollection.doc(`${this.id}`).ref.get().then((doc) => {
+        alert('Doc data is ' + doc.data().title);
+        this.article = doc.data();
+      });
   }
 
-  // getArticle(id) {
-  //   const article = this.afs.collection('articles').doc(id);
-  //   return article;
-  // }
 
   ngOnInit() {
-    this.sub = this.route.params.subscribe(params => {
-       this.ID = params['id'];
-       this.articleDoc = this.afs.doc<Article>(`articles/${this.ID}`);
-       this.article = this.articleDoc.valueChanges();
-    });
+    // this.sub = this.route.params.subscribe(params => {
+    //    this.ID = params['id'];
+         // this.articleDoc = this.afs.doc<Article>(`articles/${this.ID}`);
+         // this.article = this.articleDoc.valueChanges();
+    //        this.afs.collection('articles').doc(this.ID).ref.get().then(function(doc) {
+    //        if (doc.exists) {
+    //          this.article = doc.data();
+    //          alert('Doc data is : ' + doc.data().title);
+    //          console.log('Document data:', doc.data());
+    //        } else {
+    //          console.log('No such document!');
+    //        }
+    //        }).catch(function(error) {
+    //          console.log('Error getting document:', error);
+    //        });
+    // });
   }
 
   ngOnDestroy() {
