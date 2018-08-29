e6953ff767c0e5bafbf2ef083086296946ecbed5
John Rykken <john.rykken@gmail.com>
2018-08-29 15:08:53 -0700

    Clean up feature article component.

--- src/app/crux-feature-article/crux-feature-article.component.ts
+++ src/app/crux-feature-article/crux-feature-article.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import {
-  AngularFirestore,
-  AngularFirestoreDocument
-} from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { Router } from '@angular/router';
-import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-crux-feature-article',
@@ -14,13 +9,11 @@ import { Article } from '../models/article.model';
 })
 export class CruxFeatureArticleComponent {
   featureArticle;
-  featureArticle$: Observable<Article>;
 
-  constructor(private afs: AngularFirestore,
-              private router: Router) {
-                this.afs.doc('articles/3mjniyJYpIm7KTkhVEis').ref.get().then((doc) => {
-                  this.featureArticle = doc.data();
-                });
-                // this.featureArticle$ = this.featureArticle.valueChanges();
-             }
+  constructor(private afs: AngularFirestore, private router: Router) {
+    this.afs.doc('articles/3mjniyJYpIm7KTkhVEis').ref.get().then((doc) => {
+      this.featureArticle = doc.data();
+    });
+  }
+
 }
