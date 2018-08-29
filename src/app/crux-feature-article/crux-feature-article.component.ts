57ffaea06063d12c8d3ba00601a016003e7c4ccc
John Rykken <john.rykken@gmail.com>
2018-08-29 15:04:56 -0700

    Retrieve and display featured article.

--- src/app/crux-feature-article/crux-feature-article.component.ts
+++ src/app/crux-feature-article/crux-feature-article.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-crux-feature-article',
   templateUrl: './crux-feature-article.component.html',
   styleUrls: ['./crux-feature-article.component.scss']
 })
-export class CruxFeatureArticleComponent implements OnInit {
+export class CruxFeatureArticleComponent {
+  featureArticle;
+  featureArticle$: Observable<Article>;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private afs: AngularFirestore,
+              private router: Router) {
+                this.afs.doc('articles/3mjniyJYpIm7KTkhVEis').ref.get().then((doc) => {
+                  this.featureArticle = doc.data();
+                });
+                // this.featureArticle$ = this.featureArticle.valueChanges();
+             }
 }
